5258fe5d67707f6a4441c64db56b3ee8cb8395d1
Nhien Dang <danghaonhien@gmail.com>
2025-03-31 02:31:57 -0700

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    headers: {
+      'Content-Type': 'application/javascript',
+      'Cache-Control': 'no-cache',
+      'X-Content-Type-Options': 'nosniff'
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
